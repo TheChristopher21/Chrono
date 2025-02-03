c8f57c1635160ac51c3f9fbbae0aa913a5714ed8
TheChristopher21 <siefert.sc@gmail.com>
2025-02-03 16:26:04 +0100

    ...

--- Chrono-frontend/src/utils/api.js
+++ Chrono-frontend/src/utils/api.js
@@ -2,13 +2,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080', // Backend-URL
+    baseURL: 'http://localhost:8080',  // Backend-Port!
     headers: { 'Content-Type': 'application/json' }
 });
 
-// Automatisches Anhängen des JWT, falls vorhanden
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
