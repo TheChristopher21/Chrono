b304c8f3024a8387981132eab11881cfb2fc3866
TheChristopher21 <siefert.sc@gmail.com>
2025-06-24 13:25:25 +0200

    connect

--- Chrono-frontend/src/main.jsx
+++ Chrono-frontend/src/main.jsx
@@ -1,23 +1,25 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { NotificationProvider } from "./context/NotificationContext";
-import { LanguageProvider } from "./context/LanguageContext"; // <-- NEU
+import { LanguageProvider } from "./context/LanguageContext";
 import "./styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <HashRouter>
-        <AuthProvider>
-            <NotificationProvider>
-                <LanguageProvider>
+        {/* LanguageProvider muss die Provider umschließen, die useTranslation() verwenden */}
+        <LanguageProvider>
+            <AuthProvider>
+                <NotificationProvider>
                     <React.StrictMode>
                         <App />
                     </React.StrictMode>
-                </LanguageProvider>
-            </NotificationProvider>
-        </AuthProvider>
+                </NotificationProvider>
+            </AuthProvider>
+        </LanguageProvider>
     </HashRouter>
 );
