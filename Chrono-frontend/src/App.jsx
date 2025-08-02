35178a0036a594b71ed3f061bed899291998b76c
Christopher Siefert <128732923+TheChristopher21@users.noreply.github.com>
2025-08-02 13:37:59 +0200

    feat: add error boundary

--- Chrono-frontend/src/App.jsx
+++ Chrono-frontend/src/App.jsx
@@ -39,6 +39,7 @@ import Impressum from "./pages/Impressum.jsx";
 
 // Hilfs-Komponenten
 import PrivateRoute from "./components/PrivateRoute.jsx";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
 
 const queryClient = new QueryClient();
 
@@ -48,51 +49,53 @@ function App() {
 
     return (
         <QueryClientProvider client={queryClient}>
-            <div className="App">
-                <Routes>
-                    {/* Öffentliche Routen */}
-                    <Route path="/" element={<LandingPage />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Registration />} />
-                    <Route path="/agb" element={<AGB />} />
-                    <Route path="/impressum" element={<Impressum />} />
+            <ErrorBoundary>
+                <div className="App">
+                    <Routes>
+                        {/* Öffentliche Routen */}
+                        <Route path="/" element={<LandingPage />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Registration />} />
+                        <Route path="/agb" element={<AGB />} />
+                        <Route path="/impressum" element={<Impressum />} />
 
-                    {/* Geschützte Benutzer-Routen */}
-                    <Route path="/dashboard" element={<PrivateRoute><UserDashboard /></PrivateRoute>} />
-                    <Route path="/percentage-punch" element={<PrivateRoute><PercentagePunch /></PrivateRoute>} />
-                    <Route path="/personal-data" element={<PrivateRoute><PersonalDataPage /></PrivateRoute>} />
+                        {/* Geschützte Benutzer-Routen */}
+                        <Route path="/dashboard" element={<PrivateRoute><UserDashboard /></PrivateRoute>} />
+                        <Route path="/percentage-punch" element={<PrivateRoute><PercentagePunch /></PrivateRoute>} />
+                        <Route path="/personal-data" element={<PrivateRoute><PersonalDataPage /></PrivateRoute>} />
 
-                    {/* Admin-Routen */}
-                    <Route path="/admin/dashboard" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminDashboard /></PrivateRoute>} />
-                    <Route path="/admin/users" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminUserManagementPage /></PrivateRoute>} />
-                    <Route path="/admin/change-password" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminChangePassword /></PrivateRoute>} />
-                    <Route path="/admin/customers" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminCustomersPage /></PrivateRoute>} />
-                    <Route path="/admin/projects" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminProjectsPage /></PrivateRoute>} />
-                    <Route path="/admin/company" element={<PrivateRoute requiredRole="ROLE_ADMIN"><CompanyManagementPage /></PrivateRoute>} />
-                    <Route path="/admin/payslips" element={<PrivateRoute requiredRole={["ROLE_ADMIN","ROLE_PAYROLL_ADMIN"]}><AdminPayslipsPage /></PrivateRoute>} />
-                    <Route path="/admin/schedule" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminSchedulePlannerPage /></PrivateRoute>} />
+                        {/* Admin-Routen */}
+                        <Route path="/admin/dashboard" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminDashboard /></PrivateRoute>} />
+                        <Route path="/admin/users" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminUserManagementPage /></PrivateRoute>} />
+                        <Route path="/admin/change-password" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminChangePassword /></PrivateRoute>} />
+                        <Route path="/admin/customers" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminCustomersPage /></PrivateRoute>} />
+                        <Route path="/admin/projects" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminProjectsPage /></PrivateRoute>} />
+                        <Route path="/admin/company" element={<PrivateRoute requiredRole="ROLE_ADMIN"><CompanyManagementPage /></PrivateRoute>} />
+                        <Route path="/admin/payslips" element={<PrivateRoute requiredRole={["ROLE_ADMIN", "ROLE_PAYROLL_ADMIN"]}><AdminPayslipsPage /></PrivateRoute>} />
+                        <Route path="/admin/schedule" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminSchedulePlannerPage /></PrivateRoute>} />
 
-                    <Route path="/admin/knowledge" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminKnowledgePage /></PrivateRoute>} />
+                        <Route path="/admin/knowledge" element={<PrivateRoute requiredRole="ROLE_ADMIN"><AdminKnowledgePage /></PrivateRoute>} />
 
-                    {/* NEU: Route für die Schicht-Einstellungsseite */}
-                    <Route
-                        path="/admin/shift-rules"
-                        element={
-                            <PrivateRoute requiredRole="ROLE_ADMIN">
-                                <AdminShiftRulesPage />
-                            </PrivateRoute>
-                        }
-                    />
+                        {/* NEU: Route für die Schicht-Einstellungsseite */}
+                        <Route
+                            path="/admin/shift-rules"
+                            element={
+                                <PrivateRoute requiredRole="ROLE_ADMIN">
+                                    <AdminShiftRulesPage />
+                                </PrivateRoute>
+                            }
+                        />
 
-                    <Route path="/admin/import-times" element={<PrivateRoute requiredRole="ROLE_ADMIN"><TimeTrackingImport /></PrivateRoute>} />
-                    <Route path="/whats-new" element={<PrivateRoute><WhatsNewPage /></PrivateRoute>} />
-                    <Route path="/print-report" element={<PrintReport />} />
-                    <Route path="*" element={<Navigate to="/" replace />} />
-                </Routes>
+                        <Route path="/admin/import-times" element={<PrivateRoute requiredRole="ROLE_ADMIN"><TimeTrackingImport /></PrivateRoute>} />
+                        <Route path="/whats-new" element={<PrivateRoute><WhatsNewPage /></PrivateRoute>} />
+                        <Route path="/print-report" element={<PrintReport />} />
+                        <Route path="*" element={<Navigate to="/" replace />} />
+                    </Routes>
 
-                {/* NEU: Fügt den Chatbot (via ActionButtons) auf allen Seiten für eingeloggte User hinzu */}
-                {authToken && <ActionButtons />}
-            </div>
+                    {/* NEU: Fügt den Chatbot (via ActionButtons) auf allen Seiten für eingeloggte User hinzu */}
+                    {authToken && <ActionButtons />}
+                </div>
+            </ErrorBoundary>
         </QueryClientProvider>
     );
 }
